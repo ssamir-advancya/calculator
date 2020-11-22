8c470904c0c8bf70e04b3cb548152a6c73ddcfd2
ssamir-advancya <74568015+ssamir-advancya@users.noreply.github.com>
2020-11-22 14:34:43 +0200

    Added Comment to explain addition

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -12,7 +12,7 @@ exports.calculate = function(req, res) {
 
   var operations = {
     //using + operator to coerce variables to number.
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
